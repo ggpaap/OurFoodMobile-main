55dfc4d7c47391c260f61ae376d06324b57fb419
RafaAlbano <rafaelaalbano70@gmail.com>
2023-08-29 01:10:11 -0300

    Atualizando as rotas

--- App.js
+++ App.js
@@ -1,33 +1,13 @@
-import Home from "./src/pages/Home/index";
-import Perfil from "./src/pages/Perfil/index";
-import Header from "./src/components/Header/Header";
-import Carrinho from "./src/pages/Carrinho/index";
-import Pagamento from "./src/pages/Pagamento/index";
-import Pix from "./src/pages/Pagamento/pix";
-import Dinheiro from "./src/pages/Pagamento/dinheiro";
-import Cartao from "./src/pages/Pagamento/cartao";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Fragment } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
-const Stack = createNativeStackNavigator();
+import Routes from './src/routes';
 
-export default function app() {
-  const CustomHeader = () => <Header />;
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          header: CustomHeader,
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Carrinho" component={Carrinho} />
-        <Stack.Screen name="Pagamento" component={Pagamento} />
-        <Stack.Screen name="PIX" component={Pix} />
-        <Stack.Screen name="Dinheiro" component={Dinheiro} />
-        <Stack.Screen name="Cartao" component={Cartao} />
-        <Stack.Screen name="Perfil" component={Perfil} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Fragment>
+      <StatusBar style="auto" />
+      <Routes />
+    </Fragment>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,32 @@
-import { createStackNavigator } from '@react-navigation/stack';
-import Carrinho from "./pages/Carrinho/index";
-import Perfil from "./pages/Perfil/index";
 import Home from "./pages/Home/index";
+import Perfil from "./pages/Perfil/index";
 import Header from "./components/Header/Header";
+import Carrinho from "./pages/Carrinho/index";
+import Pagamento from "./pages/Pagamento/index";
+import Pix from "./pages/Pagamento/pix";
+import Dinheiro from "./pages/Pagamento/dinheiro";
+import Cartao from "./pages/Pagamento/cartao";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createStackNavigator();
-import { NavigationContainer } from '@react-navigation/native';
+const Stack = createNativeStackNavigator();
 
-function HeaderRoutes() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Carrinho" component={Carrinho} />
-      <Stack.Screen name="Perfil" component={Perfil} />
-    </Stack.Navigator>
-  );
-}
-
-export default function Routes() {
+export default function app() {
+  const CustomHeader = () => <Header />;
   return (
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          header: ({ navigation }) => <Header navigation={navigation} />
+          header: CustomHeader,
         }}
       >
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="HeaderRoutes" component={HeaderRoutes} /> 
+        <Stack.Screen name="Carrinho" component={Carrinho} />
+        <Stack.Screen name="Pagamento" component={Pagamento} />
+        <Stack.Screen name="PIX" component={Pix} />
+        <Stack.Screen name="Dinheiro" component={Dinheiro} />
+        <Stack.Screen name="Cartao" component={Cartao} />
+        <Stack.Screen name="Perfil" component={Perfil} />
       </Stack.Navigator>
     </NavigationContainer>
   );
