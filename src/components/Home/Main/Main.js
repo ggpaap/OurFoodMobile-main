f0b6954ea535bf012f482dab4f41076ff571b149
RafaAlbano <rafaelaalbano70@gmail.com>
2023-09-06 14:57:28 -0300

    terminar de linkar

--- src/components/Home/Main/Main.js
+++ src/components/Home/Main/Main.js
@@ -8,9 +8,24 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import styles from "./style";
 import { useNavigation } from "@react-navigation/native";
 
-export default function Main({}) {
-  const navigation = useNavigation();
+import produtoService from '../../../services/produtos';
+import { useEffect, useState } from 'react';
 
+export default function Main({}) {
+  
+  const [produtos, setProdutos] = useState([]);
+  useEffect(async () => {
+    const data = await produtoService.getAllProdutos();
+    setProdutos(data);
+  }, []);
+
+  async function updateProdutos() {
+    const data = await produtoService.getAllProdutos();
+    setProdutos(data);
+  }
+
+
+  const navigation = useNavigation();
   const combos = [
     {
       id: 1,
@@ -104,77 +119,87 @@ export default function Main({}) {
     },
   ];
   return (
-    <View style={styles.main}>
-      <View>
-        <View style={styles.categorias}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text style={styles.TextH2}>Combos</Text>
-            <TouchableOpacity onPress={() => navigation.navigate("Combos")}>
-              <View
-                style={{ flexDirection: "row", alignItems: "center", gap: 7 }}
-              >
-                <MaterialCommunityIcons
-                  name="arrow-right-bold-box"
-                  size={20}
-                  color="#FF842B"
-                />
-                <Text
-                  style={{
-                    color: "#FF842B",
-                    fontSize: 15,
-                    fontWeight: "600",
-                  }}
-                >
-                  Ver todos
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          <ScrollView horizontal={true}>
-            {combos.map((combo) => (
-              <CardCombo key={combo.id} combo={combo} />
-            ))}
-          </ScrollView>
-        </View>
-        <View style={styles.categorias}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text style={styles.TextH2}>Lanches</Text>
-          </View>
-          <ScrollView horizontal={true}>
-            {lanches.map((lanche) => (
-              <CardLanche key={lanche.id} lanche={lanche} />
-            ))}
-          </ScrollView>
-        </View>
-        <View style={styles.categorias}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text style={styles.TextH2}>Bebidas</Text>
-          </View>
-          <ScrollView horizontal={true}>
-            {bebidas.map((bebida) => (
-              <CardBebida key={bebida.id} bebida={bebida} />
-            ))}
-          </ScrollView>
-        </View>
-        <View style={styles.categorias}>
-          <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
-          >
-            <Text style={styles.TextH2}>Sobremesas</Text>
-          </View>
-          <ScrollView horizontal={true}>
-            {sobremesas.map((sobremesa) => (
-              <CardSobremesa key={sobremesa.id} sobremesa={sobremesa} />
-            ))}
-          </ScrollView>
-        </View>
-      </View>
+    <View style={styles.container}>
+      <Text>Produtos: </Text>
+      {produtos.map((produto) => (
+        <Text key={produto.id}>{produto.name}</Text>
+      ))}
+      <Button title="Atualizar" onPress={() => updateProdutos()} />
+      <StatusBar style="auto" />
     </View>
+
+
+    // <View style={styles.main}>
+    //   <View>
+    //     <View style={styles.categorias}>
+    //       <View
+    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
+    //       >
+    //         <Text style={styles.TextH2}>Combos</Text>
+    //         <TouchableOpacity onPress={() => navigation.navigate("Combos")}>
+    //           <View
+    //             style={{ flexDirection: "row", alignItems: "center", gap: 7 }}
+    //           >
+    //             <MaterialCommunityIcons
+    //               name="arrow-right-bold-box"
+    //               size={20}
+    //               color="#FF842B"
+    //             />
+    //             <Text
+    //               style={{
+    //                 color: "#FF842B",
+    //                 fontSize: 15,
+    //                 fontWeight: "600",
+    //               }}
+    //             >
+    //               Ver todos
+    //             </Text>
+    //           </View>
+    //         </TouchableOpacity>
+    //       </View>
+    //       <ScrollView horizontal={true}>
+    //         {combos.map((combo) => (
+    //           <CardCombo key={combo.id} combo={combo} />
+    //         ))}
+    //       </ScrollView>
+    //     </View>
+    //     <View style={styles.categorias}>
+    //       <View
+    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
+    //       >
+    //         <Text style={styles.TextH2}>Lanches</Text>
+    //       </View>
+    //       <ScrollView horizontal={true}>
+    //         {lanches.map((lanche) => (
+    //           <CardLanche key={lanche.id} lanche={lanche} />
+    //         ))}
+    //       </ScrollView>
+    //     </View>
+    //     <View style={styles.categorias}>
+    //       <View
+    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
+    //       >
+    //         <Text style={styles.TextH2}>Bebidas</Text>
+    //       </View>
+    //       <ScrollView horizontal={true}>
+    //         {bebidas.map((bebida) => (
+    //           <CardBebida key={bebida.id} bebida={bebida} />
+    //         ))}
+    //       </ScrollView>
+    //     </View>
+    //     <View style={styles.categorias}>
+    //       <View
+    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
+    //       >
+    //         <Text style={styles.TextH2}>Sobremesas</Text>
+    //       </View>
+    //       <ScrollView horizontal={true}>
+    //         {sobremesas.map((sobremesa) => (
+    //           <CardSobremesa key={sobremesa.id} sobremesa={sobremesa} />
+    //         ))}
+    //       </ScrollView>
+    //     </View>
+    //   </View>
+    // </View>
   );
 }
