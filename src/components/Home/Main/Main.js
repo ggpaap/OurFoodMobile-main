d5f49f9a8a112e22dfa650c44b71ad058487b97a
RafaAlbano <rafaelaalbano70@gmailcom>
2023-09-13 15:01:16 -0300

    produtos linkados

--- src/components/Home/Main/Main.js
+++ src/components/Home/Main/Main.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { View, ScrollView, TouchableOpacity } from "react-native";
 import CardCombo from "../Card/CardCombo/Card";
 import React, { Component } from "react";
 import CardLanche from "../Card/CardLanche/Card";
@@ -8,198 +8,40 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import styles from "./style";
 import { useNavigation } from "@react-navigation/native";
 
+import { Text, Button, List } from 'react-native-paper';
+
 import produtoService from '../../../services/produtos';
 import { useEffect, useState } from 'react';
+
+import { StatusBar } from 'expo-status-bar';
 
 export default function Main({}) {
   
   const [produtos, setProdutos] = useState([]);
-  useEffect(async () => {
+
+  const getProdutos = async () => {
     const data = await produtoService.getAllProdutos();
-    setProdutos(data);
+    setProdutos(data)
+  };
+
+  useEffect(async () => {
+    getProdutos();
   }, []);
 
-  async function updateProdutos() {
-    const data = await produtoService.getAllProdutos();
-    setProdutos(data);
+  const updateProdutos = async () => {
+    await getProdutos()
   }
 
-
   const navigation = useNavigation();
-  const combos = [
-    {
-      id: 1,
-      nomeProduto: "Mega combo Ourfoodq",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 2,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 3,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-  ];
-  const lanches = [
-    {
-      id: 1,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 2,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 3,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-  ];
-  const bebidas = [
-    {
-      id: 1,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 2,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 3,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-  ];
-  const sobremesas = [
-    {
-      id: 1,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 2,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-    {
-      id: 3,
-      nomeProduto: "Mega combo Ourfood",
-      valor: 30,
-      caminhoImagem: require("../../../../assets/img/images.jpeg"),
-      descricaoProduto: "Beef Burger, batata frita e refrigerante",
-    },
-  ];
+  
   return (
     <View style={styles.container}>
       <Text>Produtos: </Text>
       {produtos.map((produto) => (
-        <Text key={produto.id}>{produto.name}</Text>
+        <List.Item key={produto.id} title={produto.titulo} />
       ))}
-      <Button title="Atualizar" onPress={() => updateProdutos()} />
+      <Button title="Atualizar" icon="reload" mode="elevated" onPress={() => updateProdutos()} />
       <StatusBar style="auto" />
     </View>
-
-
-    // <View style={styles.main}>
-    //   <View>
-    //     <View style={styles.categorias}>
-    //       <View
-    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
-    //       >
-    //         <Text style={styles.TextH2}>Combos</Text>
-    //         <TouchableOpacity onPress={() => navigation.navigate("Combos")}>
-    //           <View
-    //             style={{ flexDirection: "row", alignItems: "center", gap: 7 }}
-    //           >
-    //             <MaterialCommunityIcons
-    //               name="arrow-right-bold-box"
-    //               size={20}
-    //               color="#FF842B"
-    //             />
-    //             <Text
-    //               style={{
-    //                 color: "#FF842B",
-    //                 fontSize: 15,
-    //                 fontWeight: "600",
-    //               }}
-    //             >
-    //               Ver todos
-    //             </Text>
-    //           </View>
-    //         </TouchableOpacity>
-    //       </View>
-    //       <ScrollView horizontal={true}>
-    //         {combos.map((combo) => (
-    //           <CardCombo key={combo.id} combo={combo} />
-    //         ))}
-    //       </ScrollView>
-    //     </View>
-    //     <View style={styles.categorias}>
-    //       <View
-    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
-    //       >
-    //         <Text style={styles.TextH2}>Lanches</Text>
-    //       </View>
-    //       <ScrollView horizontal={true}>
-    //         {lanches.map((lanche) => (
-    //           <CardLanche key={lanche.id} lanche={lanche} />
-    //         ))}
-    //       </ScrollView>
-    //     </View>
-    //     <View style={styles.categorias}>
-    //       <View
-    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
-    //       >
-    //         <Text style={styles.TextH2}>Bebidas</Text>
-    //       </View>
-    //       <ScrollView horizontal={true}>
-    //         {bebidas.map((bebida) => (
-    //           <CardBebida key={bebida.id} bebida={bebida} />
-    //         ))}
-    //       </ScrollView>
-    //     </View>
-    //     <View style={styles.categorias}>
-    //       <View
-    //         style={{ flexDirection: "row", justifyContent: "space-between" }}
-    //       >
-    //         <Text style={styles.TextH2}>Sobremesas</Text>
-    //       </View>
-    //       <ScrollView horizontal={true}>
-    //         {sobremesas.map((sobremesa) => (
-    //           <CardSobremesa key={sobremesa.id} sobremesa={sobremesa} />
-    //         ))}
-    //       </ScrollView>
-    //     </View>
-    //   </View>
-    // </View>
   );
 }
